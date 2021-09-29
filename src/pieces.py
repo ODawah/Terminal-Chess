d7233c4a0628c094ffc08dfa85753d47d9b5a921
ODawah <omar.sanoval550@gmail.com>
2021-09-29 20:22:36 +0200

    finished pawn tests

---
+++ src/pieces.py
@@ -0,0 +1,117 @@
+class piece:
+    def __init__(self, x, y, color):
+        self.x = x
+        self.y = y
+        self.color = color
+
+
+class Pawn():
+    def __init__(self, x, y, color):
+        self.x = x
+        self.y = y
+        self.color = color
+
+    def valid_moves(self):
+        return []
+    # def rule(self,player,x1,y1):
+    #     if player == 1:
+    #         if (self.y2 - y1 == 2) and x1 == 1:
+    #             return True
+    #         elif (self.y2 - y1 == 1):
+    #             return True
+    #         else:
+    #             print("pawn doesn't move like that")
+    #             return False
+
+    #     elif player == 2:
+    #         if (self.y2 - y1 == -2) and x1 == 6:
+    #             return True
+    #         elif (self.y2 - y1 == -1):
+    #             return True
+    #         else:
+    #             print("pawn doesn't move like that")
+    #             return False
+
+
+class Rook(piece):
+
+    def valid_moves(self):
+        return False
+    # def __init__(self, x2, y2):
+    #     self.x2 = x2
+    #     self.y2 = y2
+
+    # def rule(self, x1, y1):
+    #     if  (abs(self.x2 - x1) == 0) or (abs(self.y2 - y1) == 0):
+    #         return True
+    #     else:
+    #         return False
+
+
+class Knight(piece):
+
+    def valid_moves(self):
+        return False
+    # def __init__(self, x2, y2):
+    #     self.x2 = x2
+    #     self.y2 = y2
+
+    # def rule(self, x1, y1):
+    #     if (abs(self.y2 - y1) == 2 and abs(self.x2 - x1) == 1) or (abs(self.y2 - y1) == 1 and abs(self.x2 - x1) == 2):
+    #         return True
+    #     else:
+    #         return False
+
+
+class Bishop(piece):
+
+    def valid_moves(self):
+        return False
+    # def __init__(self, x2, y2):
+    #     self.x2 = x2
+    #     self.y2 = y2
+
+    # def rule(self, x1, y1):
+    #     if abs((x1 - self.x2) / (y1 - self.y2)):
+    #         return True
+    #     else:
+    #         return False
+
+
+class Queen(piece):
+
+    def valid_moves(self):
+        return False
+    # def __init__(self, x2, y2):
+    #     self.x2 = x2
+    #     self.y2 = y2
+
+    # def rule(self, x1, y1):
+    #     if (
+    #         (abs((x1 - self.x2) / (y1 - self.y2)) == 1)
+    #         or ((x1 - self.x2) == 0)
+    #         or ((y1 - self.y2) == 0)
+    #     ):
+    #         return True
+    #     else:
+    #         return False
+
+
+class King(piece):
+
+    def valid_moves(self):
+        return False
+
+    # def __init__(self, x2, y2):
+    #     self.x2 = x2
+    #     self.y2 = y2
+
+    # def rule(self, x1, y1):
+    #     if (self.x2 - x1 > 1) or (self.y2 - y1 > 1):
+    #         return True
+    #     else:
+    #         return False
+
+p = Pawn(0,0,"White")
+
+print(p.valid_moves)
--- test.py
+++ test.py
@@ -1,71 +1,114 @@
-from src.rules import Pawn,Rook,Bishop,King,Knight,Queen
+from src import pieces
+
 
 def example_pawn_data_W():
-    p = Pawn(1,3,"White")
+    p = pieces.Pawn(1, 3, "White")
     return p.valid_moves
+
 
 def example_pawn_data_B():
-    p = Pawn(6,7,"Black")
+    p = pieces.Pawn(6, 7, "Black")
     return p.valid_moves
 
 
-def test_pawn_free_forward_W():
-    assert example_pawn_data_W == [(2,3),(3,3)]
+class Test_pawn_white:
+    def test_pawn_free_forward_W(self):
+        assert example_pawn_data_W == [(2, 3), (3, 3)]
 
-def test_pawn_one_forward_W():
-    assert example_pawn_data_W == [(2,3)]
+    def test_pawn_one_forward_W(self):
+        assert example_pawn_data_W == [(2, 3)]
 
-def test_pawn_cant_move_W():
-    assert example_pawn_data_W == []
+    def test_pawn_cant_move_W(self):
+        assert example_pawn_data_W == []
 
-## vague
-def test_pawn_eat_pawn_W():
-    assert Pawn(2,3,"White") == [(3,2)]
+    def test_pawn_upleft_corner_W(self):
+        p = pieces.Pawn(0, 0, "White")
+        assert p.valid_moves == [(1, 0), (2, 0)]
+
+    def test_pawn_upright_corner_W(self):
+        p = pieces.Pawn(0, 7, "White")
+        assert p.valid_moves == [(1, 7), (2, 7)]
+
+    def test_pawn_downleft_one_corner_W(self):
+        p = pieces.Pawn(6, 0, "White")
+        assert p.valid_moves == [(7, 0)]
+
+    def test_pawn_downleft_two_corner_W(self):
+        p = pieces.Pawn(5, 0, "White")
+        assert p.valid_moves == [(6, 0), (7, 0)]
+
+    def test_pawn_downright_one_corner_W(self):
+        p = pieces.Pawn(6, 7, "White")
+        assert p.valid_moves == [(7, 7)]
+
+    def test_pawn_downright_two_corner_W(self):
+        p = pieces.Pawn(5, 7, "White")
+        assert p.valid_moves == [(6, 7), (7, 7)]
+
+    def test_pawn_outleft_W(self):
+        p = pieces.Pawn(2, -2, "White")
+        assert p.valid_moves == ("piece outside of the board")
+
+    def test_pawn_outright_W(self):
+        p = pieces.Pawn(0, 9, "White")
+        assert p.valid_moves == ("piece outside of the board")
+    
+    def test_pawn_outup_W(self):
+        p = pieces.Pawn(-3, 2, "White")
+        assert p.valid_moves == ("piece outside of the board")
+
+    def test_pawn_outdown_W(self):
+        p = pieces.Pawn(10, 2, "White")
+        assert p.valid_moves == ("piece outside of the board")
+
+        
 
 
+class Test_pawn_black:
+    def test_pawn_free_forward_B(self):
+        assert example_pawn_data_B == [(5, 7), (4, 7)]
 
-def test_pawn_free_forward_B():
-    assert example_pawn_data_B == [(5,7),(4,7)]
+    def test_pawn_one_forward_B(self):
+        assert example_pawn_data_B == [(5, 7)]
 
-def test_pawn_one_forward_B():
-    assert example_pawn_data_B == [(5,7)]
+    def test_pawn_cant_move_B(self):
+        assert example_pawn_data_B == []
 
-def test_pawn_cant_move_B():
-    assert example_pawn_data_B == []
-
-## vague
-def test_pawn_eat_pawn_B():
-    assert Pawn(5,7,"Black") == [(4,6)]
 
 #########################################
 
 # Rook
 
 def test_rook_blocked_intial():
-    r = Rook(0,0,"White")
+    r = pieces.Rook(0, 0, "White")
     assert r.valid_moves == []
 
+
 def test_rook_mid_blocker():
-    r = Rook(3,3,"White")
-    assert r.valid_moves == [(2,3),(3,0),(3,1),(3,2),(3,4),(4,3),(5,3)]
+    r = pieces.Rook(3, 3, "White")
+    assert r.valid_moves == [(2, 3), (3, 0), (3, 1),
+                             (3, 2), (3, 4), (4, 3), (5, 3)]
+
 
 def test_rook_mid_enemy():
-    r = Rook(4,2,"Black")
-    assert r.valid_moves == [(5,2),(4,0),(4,1),(3,2),(4,3),(4,4),(4,5),(4,6),(4,7)]
+    r = pieces.Rook(4, 2, "Black")
+    assert r.valid_moves == [(5, 2), (4, 0), (4, 1),
+                             (3, 2), (4, 3), (4, 4), (4, 5), (4, 6), (4, 7)]
 
 ########################################
 
 # King
 
+
 def King_blocked():
-    k = King(0,4,"White")
+    k = pieces.King(0, 4, "White")
     assert k.valid_moves() == []
 
-def King_blocked_move():
-    k = King(6,4,"Black")
-    assert k.valid_moves == [(7,4),(5,4),(5,5)]
-#still searching for the rules
 
+def King_blocked_move():
+    k = pieces.King(6, 4, "Black")
+    assert k.valid_moves == [(7, 4), (5, 4), (5, 5)]
+# still searching for the rules
 
 
 ###########################################
@@ -73,44 +116,56 @@ def King_blocked_move():
 # knight
 
 def test_knight_free():
-    n = Knight(4,3,"Black")
-    assert n.valid_moves == [(2,4),(2,2),(3,5),(3,1)]
+    n = pieces.Knight(4, 3, "Black")
+    assert n.valid_moves == [(2, 4), (2, 2), (3, 5), (3, 1)]
+
 
 def test_knight_blocked():
-    n = Knight(0,1,"White")
+    n = pieces.Knight(0, 1, "White")
     assert n.valid_moves == []
 
+
 def test_knight_blocked_path():
-    n = Knight(3,3,"White")
-    assert n.valid_moves == [(2,5),(5,2),(5,4)]
+    n = pieces.Knight(3, 3, "White")
+    assert n.valid_moves == [(2, 5), (5, 2), (5, 4)]
 
 ###########################################
 
 # Bishop
 
+
 def test_Bishop_blocked():
-    b = Bishop(7,2,"Black")
+    b = pieces.Bishop(7, 2, "Black")
     assert b.valid_moves == []
 
+
 def test_Bishop_path_blocked():
-    b = Bishop(3,7,"White")
-    assert b.valid_moves == [(1,5),(4,6),(5,5)]
+    b = pieces.Bishop(3, 7, "White")
+    assert b.valid_moves == [(1, 5), (4, 6), (5, 5)]
+
+
 def test_Bishop_free():
-    b = Bishop(3,5,"Black")
-    assert b.valid_moves == [(2,6),(1,7),(4,4),(5,3),(2,4),(1,3),(4,6),(5,7)]
+    b = pieces.Bishop(3, 5, "Black")
+    assert b.valid_moves == [(2, 6), (1, 7), (4, 4),
+                             (5, 3), (2, 4), (1, 3), (4, 6), (5, 7)]
 
 ###########################################
 
 # Queen
 
+
 def test_queen_blocked():
-    q = Queen(7,3,"Black")
+    q = pieces.Queen(7, 3, "Black")
     assert q.valid_moves == []
 
+
 def test_queen_free():
-    q = Queen(3,7,"White")
-    assert q.valid_moves == [(2,7),(3,6),(3,5),(3,4),(3,3),(3,2),(3,1),(3,0),(4,7),(5,7),(6,7)]
+    q = pieces.Queen(3, 7, "White")
+    assert q.valid_moves == [(2, 7), (3, 6), (3, 5), (3, 4),
+                             (3, 3), (3, 2), (3, 1), (3, 0), (4, 7), (5, 7), (6, 7)]
+
 
 def test_queen_path_limited():
-    q = Queen(3,0,"Black")
-    assert q.valid_moves == [(2,0),(1,0),(3,1),(3,2),(4,1),(5,2),(4,0),(5,0)]
+    q = pieces.Queen(3, 0, "Black")
+    assert q.valid_moves == [(2, 0), (1, 0), (3, 1),
+                             (3, 2), (4, 1), (5, 2), (4, 0), (5, 0)]
